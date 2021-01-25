92332fb842d9dedcce2a56e907e2b34d178b7047
sun-bigshan <sunbigshan@gmail.com>
2021-01-25 15:23:44 +0800

    test

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,10 +1,6 @@
 module.exports = {
-  extends: ["@commitlint/config-conventional"],
+  extends: ['@commitlint/config-conventional'],
   rules: {
-    "type-enum": [
-      2,
-      "always",
-      ["upd", "feat", "fix", "refactor", "docs", "chore", "style", "revert"]
-    ]
+    'type-enum': [2, 'always', ['upd', 'feat', 'fix', 'refactor', 'docs', 'chore', 'style', 'revert']]
   }
 };
